f91d86b4ee91defbc96090ec0ce83de935f3fdab
gifuroboken <gifuroboken@GNCTRobotClub.GNCT>
2015-04-12 11:48:08 +0900

    Moning, write page.66

--- part2/db/migrate/20150412024408_create_books.rb
+++ part2/db/migrate/20150412024408_create_books.rb
@@ -1,11 +1,11 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
-      t.string :isbn
-      t.string :title
+      t.string  :isbn
+      t.string  :title
       t.integer :price
-      t.string :publish
-      t.date :published
+      t.string  :publish
+      t.date    :published
       t.boolean :cd
 
       t.timestamps null: false
